0c37f8206313016ff4a0cc9c70e82856e9eaba14
Your Name <ahmet.dikbayir@gmail.com>
2024-03-25 00:30:36 +0300

    delete user method created

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -4,17 +4,27 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Library {
+    Scanner scan = new Scanner(System.in);
+    Connection con = DriverManager.
+            getConnection("jdbc:postgresql://localhost:5432/library", "techprodt", "password");
+
+    Statement st = con.createStatement();
+
+    public Library() throws SQLException {
+    }
+
     public static void main(String[] args) throws SQLException {
 
         Library library1 = new Library();
-        library1.createUser();
+        //library1.createUser();
+        library1.deleteUser();
 
 
 
     }
 
     public void createUser() throws SQLException {
-        Scanner scan = new Scanner(System.in);
+
         System.out.println("Please enter the member name : ");
         String name = scan.nextLine();
         System.out.println("Please enter the member lastname : ");
@@ -25,10 +35,7 @@ public class Library {
         String user_tel = scan.nextLine();
         System.out.println("User successfully added!!");
 
-        Connection con = DriverManager.
-                getConnection("jdbc:postgresql://localhost:5432/library", "techprodt", "password");
 
-        Statement st = con.createStatement();
         System.out.println("success");
         String sql = "INSERT INTO users VALUES(?,?,?,?)";
         PreparedStatement prst1 = con.prepareStatement(sql);
@@ -36,7 +43,21 @@ public class Library {
         prst1.setString(2,lastName);
         prst1.setString(3, email);
         prst1.setString(4,user_tel);
-        int updated = prst1.executeUpdate();
-        System.out.println("updated = " + updated);
+        prst1.executeUpdate();
+        st.close();
+        con.close();
     }
+
+    public void deleteUser() throws SQLException {
+        System.out.println("Pleae enter the user name:");
+        String name = scan.nextLine();
+        System.out.println("Please enter the member lastname : ");
+        String lastName = scan.nextLine();
+        String delSql = "DELETE FROM users WHERE userName = ? AND userLastname = ?";
+        PreparedStatement prst2 = con.prepareStatement(delSql);
+        prst2.setString(1,name);
+        prst2.setString(2,lastName);
+        prst2.executeUpdate();
+    }
+
 }
